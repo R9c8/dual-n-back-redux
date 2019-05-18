98eead5fc0931c2bbeaa74318e1c6d0694b0ea07
alexander_k <alkanenko@ya.ru>
2019-05-18 16:29:46 +0300

    hr style fix

--- src/ui/atoms/hr.js
+++ src/ui/atoms/hr.js
@@ -9,7 +9,7 @@ export const Hr1 = styled.hr`
 
 export const Hr2 = styled.hr`
   margin-top: 0.5rem;
-  margin-bottom: 1rem;
+  margin-bottom: 0.5rem;
   border: 0;
   border-top: 1px solid rgba(0, 0, 0, 0.1);
 `;
