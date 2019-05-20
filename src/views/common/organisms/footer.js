6033cdcf17c2fc226ead76eb82c76b1f9dcfaee0
R9c8 <alkanenko@ya.ru>
2019-05-20 21:16:29 +0300

    hide footer when game started

--- src/views/common/organisms/footer.js
+++ src/views/common/organisms/footer.js
@@ -1,19 +1,36 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-export const Footer = () => (
+export const Footer = ({ isGameStarted }) => (
   <FooterBox>
-    <FooterLink to="/terms">Terms</FooterLink>
-    <FooterLink to="/about">About</FooterLink>
-    <FooterLinkAbsolute href="https://github.com/R9c8/dual-n-back-redux">
-      GitHub
-    </FooterLinkAbsolute>
+    {!isGameStarted
+      && (
+        <>
+          <FooterLink to="/terms">Terms</FooterLink>
+          <FooterLink to="/about">About</FooterLink>
+          <FooterLinkAbsolute href="https://github.com/R9c8/dual-n-back-redux">
+            GitHub
+          </FooterLinkAbsolute>
+        </>
+      )
+    }
+
   </FooterBox>
 );
 
+Footer.propTypes = {
+  isGameStarted: PropTypes.bool,
+};
+
+Footer.defaultProps = {
+  isGameStarted: false,
+};
+
 const FooterBox = styled.footer`
   {
+    height: 35px;
     margin-top: 5px;
     margin-bottom: 15px;
     display: flex;
