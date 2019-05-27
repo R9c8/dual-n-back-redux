41e777966c2f7070462787457ae67042f7d2fc69
R9c8 <alkanenko@ya.ru>
2019-05-27 19:46:42 +0300

    link to repo updated

--- src/views/common/organisms/footer.js
+++ src/views/common/organisms/footer.js
@@ -10,7 +10,7 @@ export const Footer = ({ isGameStarted }) => (
         <>
           <FooterLink to="/terms">Terms</FooterLink>
           <FooterLink to="/about">About</FooterLink>
-          <FooterLinkAbsolute href="https://github.com/R9c8/dual-n-back-redux">
+          <FooterLinkAbsolute href="https://github.com/R9c8/n-back-training">
             GitHub
           </FooterLinkAbsolute>
         </>
