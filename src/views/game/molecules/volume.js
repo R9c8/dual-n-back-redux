26b6599396b01d6969187ba6d453b32f57dc91e1
R9c8 <alkanenko@ya.ru>
2019-05-28 21:52:08 +0300

    fix volume

--- src/views/game/molecules/volume.js
+++ src/views/game/molecules/volume.js
@@ -7,49 +7,44 @@ import Slider from 'rc-slider';
 
 import { H4 } from '../../../ui';
 
-export const Volume = ({ defaultValue, onChange, onAfterChange }) => {
-  const [value, setValue] = useState(defaultValue);
-  return (
-    <div>
-      <H4>
-        Sound volume:&nbsp;
-        {value}
-        %
-      </H4>
-      <Slider
-        defaultValue={defaultValue}
-        value={value}
-        onChange={
-          (v) => {
-            setValue(v);
-            if (onChange) {
-              onChange(v);
-            }
-          }}
-        onAfterChange={onAfterChange}
-        trackStyle={{ backgroundColor: '#00bc8c', height: 3 }}
-        handleStyle={{
-          borderColor: '#00bc8c',
-          height: 16,
-          width: 16,
-          marginLeft: -6,
-          marginTop: -6,
-          backgroundColor: 'white',
-        }}
-        railStyle={{ backgroundColor: '#222', height: 3 }}
-      />
-    </div>
-  );
-};
+export const Volume = ({
+  defaultValue, onChange, value, onAfterChange,
+}) => (
+  <div>
+    <H4>
+      Sound volume:&nbsp;
+      {value}
+      %
+    </H4>
+    <Slider
+      defaultValue={defaultValue}
+      value={value}
+      onChange={onChange}
+      onAfterChange={onAfterChange}
+      trackStyle={{ backgroundColor: '#00bc8c', height: 3 }}
+      handleStyle={{
+        borderColor: '#00bc8c',
+        height: 16,
+        width: 16,
+        marginLeft: -6,
+        marginTop: -6,
+        backgroundColor: 'white',
+      }}
+      railStyle={{ backgroundColor: '#222', height: 3 }}
+    />
+  </div>
+);
 
 Volume.propTypes = {
   defaultValue: PropTypes.number,
+  value: PropTypes.number,
   onChange: PropTypes.func,
   onAfterChange: PropTypes.func,
 };
 
 Volume.defaultProps = {
   defaultValue: 0,
+  value: 0,
   onChange: undefined,
   onAfterChange: undefined,
 };
