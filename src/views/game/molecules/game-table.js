4768a4a1a3917d24407e90a27196190c51443119
R9c8 <alkanenko@ya.ru>
2019-06-01 22:03:34 +0300

    fix lodash import

--- src/views/game/molecules/game-table.js
+++ src/views/game/molecules/game-table.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { isEqual } from "lodash";
+import isEqual from "lodash/isEqual";
 import { useStore } from "effector-react";
 import styled from "styled-components";
 
