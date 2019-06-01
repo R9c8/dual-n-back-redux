9debd1bb899d7f0fd8ca59c464d8acd7b0b4c35e
R9c8 <alkanenko@ya.ru>
2019-06-01 18:59:13 +0300

    delete garbage

--- src/views/game/organisms/settings.js
+++ src/views/game/organisms/settings.js
@@ -1,4 +1,4 @@
-import React, { useState, useReducer } from "react";
+import React, { useState } from "react";
 import styled, { css } from "styled-components";
 
 import { useStore } from 'effector-react';
